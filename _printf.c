80f24d7e8313c0a30d11f64564d6aa6ff370f469
dekuu5 <deku332211@gmail.com>
2023-07-25 20:42:21 +0300

    made printf to print some chars

--- _printf.c
+++ _printf.c
@@ -1,8 +1,25 @@
 #include "main.h"
 
 
-
-int _printf(char * buffer){
+char * handlePercentage(char * s)
+{
     
+}
+
+int _printf(char * buffer, char *str1){
+    int outedBuffer = 0;
+    int i = 0;
+
+    while (buffer[i] != '\0')
+    {
+        
+        if(buffer[i] == '%')
+        {
+            handlePercentage(str1);
+        }
+        _putchar(buffer[i++]);
+        outedBuffer++;
+    }
+    return outedBuffer;
 
 }
---
+++ main.h
@@ -0,0 +1,7 @@
+#ifndef MY_HEADER_H
+# define MY_HEADER_H
+
+int _printf(char *buffer);
+int _putchar(char c);  
+
+#endif
