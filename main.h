24e4c635c71dcab2a49ea9e83d40af1b90b9d701
dekuu5 <deku332211@gmail.com>
2023-07-26 01:17:54 +0300

    done with va

--- main.h
+++ main.h
@@ -1,7 +1,14 @@
 #ifndef MY_HEADER_H
 # define MY_HEADER_H
+#include <stdarg.h>
 
-int _printf(char * buffer, char *str1);
+int _printf(const char * buffer, ...);
 int _putchar(char c);  
+
+typedef struct functionMap
+{
+	char *type;
+	int (*func)(va_list);
+} fMap;
 
 #endif
